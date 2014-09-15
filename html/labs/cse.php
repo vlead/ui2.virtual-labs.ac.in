5d9574a554bf78c10bea9d6c355c42b4d9104db8
erzaliator <kaveri.anuranjana@research.iiit.ac.in>
2014-09-15 22:58:07 +0530

    images/ added and labs/cse.php changed

--- html/labs/cse.php
+++ html/labs/cse.php
@@ -2,163 +2,163 @@
 <ul align="center" class="thumbnails">
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/cse28/"><img src="img/Advanced-Network-Technologies.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/cse28/"><img src="images/ant1.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/cse28/" text-decoration="none">Advanced Network Technologies </a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse30/"><img src="img/Advanced-Network-Technologies.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse30/"><img src="images/acvcd2.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse30/" text-decoration="none">Analog CMOS VLSI Circuit design</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse22/"><img src="img/Artificial-Neural-Networks.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse22/"><img src="images/ann3.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse22/" text-decoration="none">Artificial Neural Networks</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse23/"><img src="img/Artificial-Neural-Networks.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse23/"><img src="images/cl4.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse23/" text-decoration="none">Computational Linguistics</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse10/"><img src="img/Computer-Architecture-&-organization.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse10/"><img src="images/caao5.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse10/" text-decoration="none">Computer Architecture & organization </a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://deploy.virtual-labs.ac.in/labs/cse18/index.php"><img src="img/Computer-Graphics.png" alt=""></a>
+<a href="http://deploy.virtual-labs.ac.in/labs/cse18/index.php"><img src="images/cgr6.jpg" alt=""></a>
 <h5><a href="http://deploy.virtual-labs.ac.in/labs/cse18/index.php" text-decoration="none">Computer Graphics</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse11/"><img src="img/Computer-Organization.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse11/"><img src="images/cso7.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse11/" text-decoration="none">Computer Organization</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://deploy.virtual-labs.ac.in/labs/cse02/index.php"><img src="img/Computer-Programming.png" alt=""></a>
+<a href="http://deploy.virtual-labs.ac.in/labs/cse02/index.php"><img src="images/cp8.jpg" alt=""></a>
 <h5><a href="http://deploy.virtual-labs.ac.in/labs/cse02/index.php" text-decoration="none">Computer Programming</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="experiment_cse/cryptography/cryptography-main.php"><img src="img/Cryptography.png" alt=""></a>
+<a href="experiment_cse/cryptography/cryptography-main.php"><img src="images/c9.jpg" alt=""></a>
 <h5><a href="experiment_cse/cryptography/cryptography-main.php" text-decoration="none">Cryptography</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse06/"><img src="img/Databases.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse06/"><img src="images/dm10.png" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse06/" text-decoration="none">Data Mining</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://deploy.virtual-labs.ac.in/labs/cse01/index.php"><img src="img/Databases.png" alt=""></a>
+<a href="http://deploy.virtual-labs.ac.in/labs/cse01/index.php"><img src="images/ds11.jpg" alt=""></a>
 <h5><a href="http://deploy.virtual-labs.ac.in/labs/cse01/index.php" text-decoration="none">Data Structures</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse07/"><img src="img/Databases.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse07/"><img src="images/d12.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse07/" text-decoration="none">Databases</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse15/"><img src="img/Digital-Logic-Design.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse15/"><img src="images/dld13.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse15/" text-decoration="none">Digital Logic Design</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse12/"><img src="img/FPGA-&-Embedded-systems-lab.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse12/"><img src="images/fpga14.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse12/" text-decoration="none">FPGA & Embedded systems lab</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://conf.cse.iitd.ac.in/moodle"><img src="img/FPGA-&-Embedded-systems-lab.png" alt=""></a>
+<a href="http://conf.cse.iitd.ac.in/moodle"><img src="images/fpga15.jpg" alt=""></a>
 <h5><a href="http://conf.cse.iitd.ac.in/moodle" text-decoration="none">FPGA and Digital Design lab</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse19/"><img src="img/Image-Processing.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse19/"><img src="images/ip16.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse19/" text-decoration="none">Image Processing</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://deploy.virtual-labs.ac.in/labs/cse09/"><img src="img/FPGA-&-Embedded-systems-lab.png" alt=""></a>
+<a href="http://deploy.virtual-labs.ac.in/labs/cse09/"><img src="images/ll17.jpg" alt=""></a>
 <h5><a href="http://deploy.virtual-labs.ac.in/labs/cse09/" text-decoration="none">Linux Lab</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href=" http://deploy.virtual-labs.ac.in/labs/cse17/index.php"><img src="img/Mobile-Robotics.png" alt=""></a>
+<a href=" http://deploy.virtual-labs.ac.in/labs/cse17/index.php"><img src="images/mr18.jpg" alt=""></a>
 <h5><a href="http://deploy.virtual-labs.ac.in/labs/cse17/index.php" text-decoration="none">Mobile Robotics</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse24/"><img src="img/Natural-Language-Processing.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse24/"><img src="images/nlp19.GIF" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse24/" text-decoration="none">Natural Language Processing</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://deploy.virtual-labs.ac.in/labs/cse05/"><img src="img/Natural-Language-Processing.png" alt=""></a>
+<a href="http://deploy.virtual-labs.ac.in/labs/cse05/"><img src="images/popl20.jpg" alt=""></a>
 <h5><a href="http://deploy.virtual-labs.ac.in/labs/cse05/" text-decoration="none">POPL</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse20/"><img src="img/Pattern-Recognition.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse20/"><img src="images/pt21.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse20/" text-decoration="none">Pattern Recognition</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse04"><img src="img/Natural-Language-Processing.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse04"><img src="images/ps22.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse04" text-decoration="none">Problem Solving</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://cse.iitkgp.ac.in/~rkumar/pds-vlab/"><img src="img/Natural-Language-Processing.png" alt=""></a>
+<a href="http://cse.iitkgp.ac.in/~rkumar/pds-vlab/"><img src="images/pnds23.jpg" alt=""></a>
 <h5><a href="http://cse.iitkgp.ac.in/~rkumar/pds-vlab/" text-decoration="none">Programming and DataStructures</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/cse08/"><img src="img/Software-Engineering.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/cse08/"><img src="images/se24.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/cse08/" text-decoration="none">Software Engineering</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse16/"><img src="img/Speech-Signal-Processing.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse16/"><img src="images/ssp25.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse16/" text-decoration="none">Speech Signal Processing</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse14/"><img src="img/VLSI.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse14/"><img src="images/vlsi26.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse14/" text-decoration="none">VLSI</a></h5>
 </div>
 </li>
 <li class="span2">
 <div class="thumbnail">
-<a href="http://virtual-labs.ac.in/labs/cse13/"><img src="img/VLSI.png" alt=""></a>
+<a href="http://virtual-labs.ac.in/labs/cse13/"><img src="images/vlsial27.jpg" alt=""></a>
 <h5><a href="http://virtual-labs.ac.in/labs/cse13/" text-decoration="none">Virtual Advanced VLSI lab</a></h5>
 </div>
 </li>
