c597d8df1abab17a88b17bf074c505d7c4255663
erzaliator <kaveri.anuranjana@research.iiit.ac.in>
2014-09-26 17:35:38 +0530

    Sample responsive template

---
+++ html/course2.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+<html>
+
+ <head>
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <meta charsetr="etf-8">
+	 <link href="http://fonts.googleapis.com/css?family=Oleo+Script:400" rel="stylesheet" type="text/css">
+	 <title>Virtual Labs</title>
+	 <link rel="stylesheet" href="css/bootstrap2.min.css">
+	 <link rel="stylesheet" href="css/bootstrap.global.css">
+	 
+<style type="text/css">
+
+    .input-append button.add-on {
+        height: inherit !important;
+    }
+</style>
+ </head>
+
+ <body>
+ <div class="container-fluid">
+ 	<!--=======================Navbar==================================-->
+ 	<?php include 'header2.html'; ?>
+ 	<!--=======================Navbar ends=============================-->
+
+ 	<div class="row"><!--contains both navbar and content-->
+ 	<!--=======================Main Container==========================-->
+ 	<div class="greycontainer">
+ 	<div class="col-md-8 col-md-push-3 col-sm-8 col-sm-push-3 radius-and-box lightgreycontainer">
+ 	 	
+  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+ 	<p>Lorem Ipsum is simply dummy tehahxt of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+	<div class="row">
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+	  <div class="col-xs-6 col-sm-6 col-md-4">
+	  <a href="#" class="thumbnail">
+	  <img src="images/acvcd2.jpg" alt="...">
+	  </a>
+	  </div>
+ 	</div><!--end of row-->
+ 	</div><!--end of lightgreycontainer-->
+ 	
+ 	<!--=======================Main Container ends=====================-->
+ 	
+
+ 	<!--=======================Left Navbar=============================-->
+ 	
+ 	<div class="nav nav-tabs nav-stacked">
+ 	<div class="col-md-2 col-md-pull-8 col-sm-2 col-sm-pull-8 grey-box radius-and-box">
+ 	<h4>Courses</h4>
+
+		<ul>
+			<li id="lF_menu" class="menuitem"><a href="#lF" data-toggle="tab">Biotechnology and Biomedical Engineering</a></li>
+            <li id="lD_menu" class="menuitem"><a href="#lD" data-toggle="tab">Chemical Engineering</a></li>
+            <li id="lJ_menu" class="menuitem"><a href="#lJ" data-toggle="tab">Chemical Sciences</a></li>
+            <li id="lC_menu" class="menuitem"><a href="#lC" data-toggle="tab">Civil Engineering</a></li>
+            <li id="lA_menu" class="menuitem"><a href="#lA" data-toggle="tab">Computer Science</a></li>
+            <li id="lE_menu" class="menuitem"><a href="#lE" data-toggle="tab">Electrical Engineering</a></li>
+            <li 	id="lB_menu" class="menuitem"><a href="#lB" data-toggle="tab">Electronics Engineering</a></li>
+            <li id="lI_menu" class="menuitem"><a href="#lI" data-toggle="tab">Mechanical Engineering</a></li>
+            <li id="lH_menu" class="menuitem"><a href="#lH" data-toggle="tab">Physical Sciences</a></li>
+		</ul>
+ 	
+ 	</div><!--end of left-nav bar-->
+ 	</div><!--end of nav-bar's container-->
+ 	</div><!--end of greycontainer-->
+ 	<!--=======================Left Navbar ends========================-->
+ 	</div><!--end of row containing both nav-bar and content-->
+
+ 	<!--=======================Footer==================================-->
+ 	<!--footer will be here-->
+ 	<!--=======================Footer ends=============================-->
+ </div><!--end of container fluid-->
+ </body>
+
+
+<!--========================Scripts====================================-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-modal.js"></script>
+<script src="js/bootstrap-modalmanager.js"></script> 
+
+</html>
